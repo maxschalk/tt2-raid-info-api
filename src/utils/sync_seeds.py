e0af549c463fc19e04e98a792633970e534c6c52
Maximilian Schalk <maximilian.schalk@audi.de>
2022-06-06 00:41:33 +0200

    setup sync util

---
+++ src/utils/sync_seeds.py
@@ -0,0 +1,11 @@
+
+# TODO
+
+def down():
+    # TODO Get filenames from server and download missing to local
+    pass
+
+
+def up():
+    # TODO Get filenames from server and upload missing
+    pass
