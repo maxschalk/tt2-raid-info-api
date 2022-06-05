660fb14bc93888a16d55e5cfdca7d07a56f5e045
Maximilian Schalk <maximilian.schalk@audi.de>
2022-06-06 00:35:40 +0200

    fix post seed test

--- test/manual/post_seed.py
+++ test/manual/post_seed.py
@@ -7,14 +7,10 @@ from test.manual.base import make_request
 with open(f"./test_data_20220220.json", 'r') as file:
     seed_data = json.load(file)
 
-file_data = {
-    "data": seed_data
-}
-
 result = make_request(
     requests.post,
     "admin/add_raw_seed_file/test_data_20220220.json",
-    data=json.dumps(file_data)
+    data=json.dumps(seed_data)
 )
 
 print(result)
