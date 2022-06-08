d9affbc714bf6c8864f5c04f989c0da769200162
Maximilian Schalk <maximilian.schalk@audi.de>
2022-06-08 12:05:36 +0200

    test structure

--- test/e2e/conftest.py
+++ test/e2e/conftest.py
@@ -1,17 +1,6 @@
-import os
-
-import pytest
 from dotenv import load_dotenv
 
-from src.models.Stage import Stage
-
 load_dotenv()
-
-
-@pytest.fixture(scope="module", autouse=True)
-def stage() -> Stage:
-    return Stage(os.getenv("STAGE"))
-
 
 # @pytest.fixture(scope="module", autouse=True)
 # def seeds(stage: Stage) -> List[RaidRawSeedData]:
