ce36e6baaf35c3a86bb01d7344fdf71f03defc87
Maximilian Schalk <maximilian.schalk@finn.auto>
2022-06-10 18:37:12 +0200

    fix parsing for correct datatypes

--- src/utils/safe_parse.py
+++ src/utils/safe_parse.py
@@ -13,12 +13,21 @@ def _safe_parse_base(from_str: str, constructor: Callable, default: Any = None) 
 
 
 def safe_parse_datetime(from_str: str, default: datetime = None) -> Optional[datetime]:
+    if type(from_str) == datetime:
+        return from_str
+
     return _safe_parse_base(from_str, datetime.fromisoformat, default)
 
 
 def safe_parse_int(from_str: str, default: int = None) -> Optional[int]:
+    if type(from_str) == int:
+        return from_str
+
     return _safe_parse_base(from_str, int, default)
 
 
 def safe_parse_float(from_str: str, default: float = None) -> Optional[float]:
+    if type(from_str) == float:
+        return from_str
+
     return _safe_parse_base(from_str, float, default)
