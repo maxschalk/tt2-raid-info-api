e69a93b118c620c311899f999f49a4fa6e8c0e43
Maximilian Schalk <maximilian.schalk@finn.auto>
2022-06-12 17:27:18 +0200

    refactored paths

--- test/test_seeds/test_seeds.py
+++ test/test_seeds/test_seeds.py
@@ -1,17 +1,16 @@
 import asyncio
 import operator
 import unittest
+from test.utils.make_request import make_request_async, make_request_sync
 from typing import Type
 
 import aiohttp
 import pytest
 import requests
-
+from src.models.raid_data import RaidEnhancedSeedData, RaidRawSeedData
 from src.models.SeedType import SeedType
 from src.models.SortOrder import SortOrder
 from src.models.Stage import Stage
-from src.models.raid_data import RaidRawSeedData, RaidEnhancedSeedData
-from test.utils.make_request import make_request_sync, make_request_async
 
 BASE_PATH = "seeds"
 
@@ -23,7 +22,7 @@ def seeds_all_valid_model_base(
 ):
     response = make_request_sync(
         method=requests.get,
-        path=f"{BASE_PATH}/all/{seed_type.value}",
+        path=f"{BASE_PATH}/{seed_type.value}/all",
         stage=stage,
         parse_response=False
     )
@@ -50,7 +49,7 @@ def seeds_all_sort_order_base(stage: Stage, seed_type: SeedType, sort_order: Sor
 
     response = make_request_sync(
         method=requests.get,
-        path=f"{BASE_PATH}/all/{seed_type.value}{qs}",
+        path=f"{BASE_PATH}/{seed_type.value}/all{qs}",
         stage=stage,
         parse_response=False
     )
@@ -59,14 +58,16 @@ def seeds_all_sort_order_base(stage: Stage, seed_type: SeedType, sort_order: Sor
 
     server_seeds = response.json()
 
-    server_seeds_valid_from_dates = list(map(lambda seed: seed[0]["raid_info_valid_from"], server_seeds))
+    server_seeds_valid_from_dates = list(
+        map(lambda seed: seed[0]["raid_info_valid_from"], server_seeds))
 
     if sort_order in {None, SortOrder.ASCENDING}:
         op = operator.le
     else:
         op = operator.ge
 
-    assert all(op(a, b) for a, b in zip(server_seeds_valid_from_dates, server_seeds_valid_from_dates[1:]))
+    assert all(op(a, b) for a, b in zip(
+        server_seeds_valid_from_dates, server_seeds_valid_from_dates[1:]))
 
 
 def test_seeds_all_raw_default_is_ascending(stage: Stage):
@@ -100,7 +101,7 @@ def seeds_most_recent_valid_model_base(
 ):
     response = make_request_sync(
         method=requests.get,
-        path=f"{BASE_PATH}/most_recent/{seed_type.value}",
+        path=f"{BASE_PATH}/{seed_type.value}/most_recent",
         stage=stage,
         parse_response=False
     )
@@ -118,7 +119,8 @@ def test_seeds_most_recent_raw_valid_model(stage: Stage):
 
 
 def test_seeds_most_recent_enhanced_valid_model(stage: Stage):
-    seeds_most_recent_valid_model_base(stage, SeedType.ENHANCED, RaidEnhancedSeedData)
+    seeds_most_recent_valid_model_base(
+        stage, SeedType.ENHANCED, RaidEnhancedSeedData)
 
 
 @pytest.mark.asyncio
@@ -126,13 +128,13 @@ async def test_seeds_most_recent_get_descending_seeds(stage: Stage):
     for seed_type in SeedType:
         all_server_seeds = make_request_sync(
             method=requests.get,
-            path=f"{BASE_PATH}/all/{seed_type.value}?sort_order={SortOrder.DESCENDING.value}",
+            path=f"{BASE_PATH}/{seed_type.value}/all?sort_order={SortOrder.DESCENDING.value}",
             stage=stage,
             parse_response=False
         ).json()
 
         paths = tuple(
-            f"{BASE_PATH}/most_recent/{seed_type.value}?offset_weeks={i}"
+            f"{BASE_PATH}/{seed_type.value}/most_recent?offset_weeks={i}"
             for i, _ in enumerate(all_server_seeds)
         )
 
