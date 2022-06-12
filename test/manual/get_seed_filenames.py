ea94a5a3e82500a847f4e0b686f38aef9914bd34
Maximilian Schalk <maximilian.schalk@finn.auto>
2022-06-12 15:23:44 +0200

    fix manual test paths

--- test/manual/get_seed_filenames.py
+++ test/manual/get_seed_filenames.py
@@ -1,13 +1,13 @@
-import requests
+from test.utils.make_request import make_request_sync
 
+import requests
 from src.models.SeedType import SeedType
 from src.models.Stage import Stage
-from test.utils.make_request import make_request_sync
 
 result = make_request_sync(
     method=requests.get,
-    path="admin/all_seed_filenames/raw",
-    stage=Stage.DEV
+    path=f"admin/all_seed_filenames/{SeedType.RAW.value}",
+    stage=Stage.PRODUCTION
 )
 
 print("RAW:", result)
@@ -15,7 +15,7 @@ print("RAW:", result)
 result = make_request_sync(
     method=requests.get,
     path=f"admin/all_seed_filenames/{SeedType.ENHANCED.value}",
-    stage=Stage.DEV
+    stage=Stage.PRODUCTION
 )
 
 print("ENHANCED:", result)
