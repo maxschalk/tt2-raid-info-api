f4dba2a44a531c30aed42dd0b917a979257923cf
Maximilian Schalk <maximilian.schalk@audi.de>
2022-06-18 13:42:11 +0200

    add autopep8

--- src/PATHS.py
+++ src/PATHS.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+from src.models.SeedType import SeedType
+
 ROOT_DIR = Path(__file__).parent.parent
 
 SRC_DIR = ROOT_DIR.joinpath('src')
 
 DATA_DIR = ROOT_DIR.joinpath('db')
-RAW_SEEDS_DIR = DATA_DIR.joinpath('raw_seeds')
-ENHANCED_SEEDS_DIR = DATA_DIR.joinpath('enhanced_seeds')
+RAW_SEEDS_DIR = DATA_DIR.joinpath(SeedType.RAW.value)
+ENHANCED_SEEDS_DIR = DATA_DIR.joinpath(SeedType.ENHANCED.value)
