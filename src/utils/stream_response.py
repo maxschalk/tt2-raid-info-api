5703c9ddba6d7754c6fb0e1eef7b9b26fd24d840
riskypenguin <maximilian-schalk@web.de>
2022-07-30 23:51:48 +0200

    fix: handle raid data objects correctly

--- src/utils/stream_response.py
+++ src/utils/stream_response.py
@@ -3,6 +3,7 @@ import json
 from typing import Any
 
 from fastapi.responses import StreamingResponse
+from src.model.raid_data import map_to_native_object
 
 
 def create_stream_response(*,
@@ -10,7 +11,7 @@ def create_stream_response(*,
                            filename: str = "data") -> StreamingResponse:
     stream = io.StringIO()
 
-    json.dump(data, stream)
+    json.dump(map_to_native_object(data=data), stream)
 
     return StreamingResponse(
         iter([stream.getvalue()]),
