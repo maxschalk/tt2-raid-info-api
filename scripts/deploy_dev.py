d6c4b0200005f53b8ecd17996aa38e60ec5632fc
riskypenguin <maximilian-schalk@web.de>
2022-07-31 09:31:44 +0200

    add script to deploy from dev

---
+++ scripts/deploy_dev.py
@@ -0,0 +1,43 @@
+import contextlib
+import subprocess
+
+
+@contextlib.contextmanager
+def stashed_changes():
+    subprocess.run(("git", "stash"), check=False)
+
+    try:
+        yield
+    finally:
+        subprocess.run(("git", "stash", "apply"), check=False)
+
+
+@contextlib.contextmanager
+def main_branch():
+    subprocess.run(("git", "switch", "main"), check=False)
+
+    try:
+        yield
+    finally:
+        subprocess.run(("git", "switch", "-"), check=False)
+
+
+def main():
+    cmds = (
+        ("git", "merge", "dev"),
+        ("git", "push"),
+    )
+
+    with stashed_changes():
+        with main_branch():
+            for cmd in cmds:
+                print(str.join(' ', cmd), flush=True)
+
+                try:
+                    subprocess.run(cmd, check=True)
+                except subprocess.CalledProcessError:
+                    break
+
+
+if __name__ == '__main__':
+    main()
