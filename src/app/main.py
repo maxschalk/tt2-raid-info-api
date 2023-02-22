c92c8868fd2e7b7114c17f960d5b6181ba7d09ff
riskypenguin <maximilian-schalk@web.de>
2023-02-22 13:09:26 +0100

    bump minor version

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
+
 from src.domain.seed_data_repository import SeedDataRepository
 from src.stage import Stage
 
@@ -13,7 +14,7 @@ DESCRIPTION = """
 
 def create_app(*, stage: Stage, seed_data_repo: SeedDataRepository):
     app = FastAPI(title=f"TT2 Raid Data API | {stage.value}",
-                  version="0.1.0",
+                  version="0.1.1",
                   description=DESCRIPTION,
                   swagger_ui_parameters={
                       "defaultModelsExpandDepth": -1,
